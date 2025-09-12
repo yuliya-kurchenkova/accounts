8e5f31f13657fe609016381273bd99aa5ac1d6f6
Julia Kurchenkova <iulia.kurchenkova@yandex.by>
2025-09-13 02:15:57 +0300

    install project

---
+++ nuxt.config.ts
@@ -0,0 +1,23 @@
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: false },
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+   modules: [
+    '@nuxt/icon',
+    '@pinia/nuxt'
+  ],
+   icon: {
+    mode: 'svg',
+    aliases: {
+      del: 'heroicons:archive-box-x-mark-solid',
+      },
+  },
+})
